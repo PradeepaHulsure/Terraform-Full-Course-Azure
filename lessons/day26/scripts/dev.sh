06b885a47228c5e96b86178550dcefee2704310d
Pradeepa <pradeepa.hulsure@hotmail.com>
2025-12-01 21:49:17 +0530

    change is dev.sh

--- lessons/day26/scripts/dev.sh
+++ lessons/day26/scripts/dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 RESOURCE_GROUP_NAME=terraform-state-rg
-STAGE_SA_ACCOUNT=tfstagebackend2024piyush
-DEV_SA_ACCOUNT=tfdevbackend2024piyush
-CONTAINER_NAME=tfstate
-
+STAGE_SA_ACCOUNT=tf-stage-backend
+DEV_SA_ACCOUNT=tf-dev-backend
+DEV_CONTAINER_NAME=devtfstate
+STG_CONTAINER_NAME=stgtfstate
 
 # Create resource group
 az group create --name $RESOURCE_GROUP_NAME --location canadacentral
@@ -16,7 +16,7 @@ az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STAGE_SA
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $DEV_SA_ACCOUNT --sku Standard_LRS --encryption-services blob
 
 # Create blob container for staging environment
-az storage container create --name $CONTAINER_NAME --account-name $STAGE_SA_ACCOUNT
+az storage container create --name $STG_CONTAINER_NAME --account-name $STAGE_SA_ACCOUNT
 
 # Create blob container for dev environment
-az storage container create --name $CONTAINER_NAME --account-name $DEV_SA_ACCOUNT
+az storage container create --name $DEV_CONTAINER_NAME --account-name $DEV_SA_ACCOUNT
