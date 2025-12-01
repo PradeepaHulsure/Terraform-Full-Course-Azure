3633af8543dee12c426fafe3326b744f9c9ed174
Pradeepa <pradeepa.hulsure@hotmail.com>
2025-12-01 21:57:41 +0530

    changes in dev and stg backend.tf file

--- lessons/day26/dev/backend.tf
+++ lessons/day26/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfdevbackend2024piyush"
-    container_name      = "tfstate"
+    storage_account_name = "tf-dev-backend"
+    container_name      = "devtfstate"
     key                 = "dev.tfstate"
   }
 }
--- lessons/day26/staging/backend.tf
+++ lessons/day26/staging/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstagebackend2024piyush"
-    container_name      = "tfstate"
+    storage_account_name = "tf-stage-backend"
+    container_name      = "stgtfstate"
     key                 = "stage.tfstate"
   }
 }
